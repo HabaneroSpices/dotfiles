dafadb6139002a89397edcc433b60af5faad8849
HabaneroSpices <habanerospices@gmail.com>
2024-05-31 19:53:36 +0200

    Sync : DESKTOP-J90IA5E

--- zsh/05-environment.zsh
+++ zsh/05-environment.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 if test -n "${WSL_DISTRO_NAME}"; then
-  export PATH=$PATH:/mnt/c/Windows
+  export PATH=$PATH:/mnt/c/Windows:"/mnt/c/Program Files/WindowsApps/MicrosoftCorporationII.WindowsSubsystemForLinux_2.0.4.0_x64__8wekyb3d8bbwe":"/mnt/c/Program Files/Neovim/bin"
 fi
